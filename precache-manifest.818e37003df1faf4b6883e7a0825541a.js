1c30a410e2e9cbb59996014af835a496a7e69224
Steve MacDougall <hello@designedbysteve.com>
2020-06-01 10:53:46 -0300

    Updates

---
+++ precache-manifest.818e37003df1faf4b6883e7a0825541a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30d6cdcda4a105db9d4f5c2556cf4be",
+    "url": "/run-log/index.html"
+  },
+  {
+    "revision": "f8edb65d535c82f50235",
+    "url": "/run-log/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d6952884c10c9a3cd6a2",
+    "url": "/run-log/static/css/main.f83caf34.chunk.css"
+  },
+  {
+    "revision": "f8edb65d535c82f50235",
+    "url": "/run-log/static/js/2.ce7679ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/run-log/static/js/2.ce7679ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6952884c10c9a3cd6a2",
+    "url": "/run-log/static/js/main.92445730.chunk.js"
+  },
+  {
+    "revision": "288ce86ae7de9282e199",
+    "url": "/run-log/static/js/runtime-main.f1d2b551.js"
+  }
+]);
